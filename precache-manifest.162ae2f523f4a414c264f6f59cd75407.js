01331cbd24afa4393a6570bf61b54b629f7ecdcc
upcliucong <upcliucong@gmail.com>
2019-05-14 19:36:50 +0800

    Updates

---
+++ precache-manifest.162ae2f523f4a414c264f6f59cd75407.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0db8b9e08a030ff703d8",
+    "url": "/react-colors-out/static/js/runtime~main.0db8b9e0.js"
+  },
+  {
+    "revision": "f35f31074d91ec24ae9a",
+    "url": "/react-colors-out/static/js/main.f35f3107.chunk.js"
+  },
+  {
+    "revision": "207e5d056e3105dcde38",
+    "url": "/react-colors-out/static/js/1.207e5d05.chunk.js"
+  },
+  {
+    "revision": "f35f31074d91ec24ae9a",
+    "url": "/react-colors-out/static/css/main.99ae5857.chunk.css"
+  },
+  {
+    "revision": "11e30f4df76faf76947f3672847e2b6d",
+    "url": "/react-colors-out/index.html"
+  }
+];
